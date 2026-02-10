437b1ef71c58fb538b7b2c490756c9b0524ae9bc
FatheenRahman <fatheenrahman@gmail.com>
2026-02-10 12:38:27 +0530

    Debug line added in api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
+console.log("URBAN HARVEST API URL:", API_URL); // Debugging log
 
 const api = axios.create({
     baseURL: API_URL,
